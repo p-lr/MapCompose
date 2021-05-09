1903987bb26fcf6bdef0fb5eb7ba16e23e64adab
plr <plr.devs@gmail.com>
2021-05-09 21:53:03 +0200

    Nice callout animation

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/CenteringOnMarkerDemo.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/CenteringOnMarkerDemo.kt
@@ -1,17 +1,26 @@
 package ovh.plrapps.mapcompose.demo.ui.screens
 
+import android.animation.TimeInterpolator
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -32,7 +41,7 @@ fun CenteringOnMarkerDemo(
     /* Add a callout on marker click */
     viewModel.state.apply {
         onMarkerClick { id, x, y ->
-            addCallout(id, x, y, absoluteOffset = Offset(0f, -130f), autoDismiss = false) {
+            addCallout(id, x, y, absoluteOffset = Offset(0f, -130f), autoDismiss = true) {
                 Callout(x, y)
             }
         }
@@ -51,10 +60,30 @@ fun CenteringOnMarkerDemo(
     }
 }
 
+/**
+ * A callout which animates its entry with an overshoot scaling interpolator.
+ */
 @Composable
 fun Callout(x: Double, y: Double) {
+    var animVal by remember { mutableStateOf(0f) }
+    LaunchedEffect(key1 = true) {
+        Animatable(0f).animateTo(
+            targetValue = 1f,
+            animationSpec = tween(250, easing = overshootEasing)
+        ) {
+            animVal = value
+        }
+    }
     Surface(
-        Modifier.padding(10.dp),
+        Modifier
+            .alpha(animVal)
+            .padding(10.dp)
+            .size(150.dp, 70.dp)
+            .graphicsLayer {
+                scaleX = animVal
+                scaleY = animVal
+                transformOrigin = TransformOrigin(0.5f, 1f)
+            },
         shape = RoundedCornerShape(5.dp),
         elevation = 10.dp
     ) {
@@ -71,7 +100,7 @@ fun Callout(x: Double, y: Double) {
                 text = "position ${df.format(x)} , ${df.format(y)}",
                 modifier = Modifier
                     .align(alignment = Alignment.CenterHorizontally)
-                    .padding(4.dp),
+                    .padding(top = 4.dp),
                 fontSize = 12.sp,
                 textAlign = TextAlign.Center,
                 color = Color.Black
@@ -82,4 +111,10 @@ fun Callout(x: Double, y: Double) {
 
 val df = DecimalFormat("#.##").apply {
     roundingMode = RoundingMode.CEILING
+}
+
+private val overshootEasing = OvershootInterpolator(1.2f).toEasing()
+
+private fun TimeInterpolator.toEasing() = Easing { x ->
+    getInterpolation(x)
 }
