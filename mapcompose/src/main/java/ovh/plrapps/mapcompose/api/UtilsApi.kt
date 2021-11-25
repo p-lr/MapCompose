bd8c69ace6dfb01775ecc15ae14c5aa9f45c1562
plr <plr.devs@gmail.com>
2021-11-25 20:34:32 +0100

    New rotatePoint util API

---
+++ mapcompose/src/main/java/ovh/plrapps/mapcompose/api/UtilsApi.kt
@@ -0,0 +1,26 @@
+package ovh.plrapps.mapcompose.api
+
+import ovh.plrapps.mapcompose.ui.state.MapState
+import ovh.plrapps.mapcompose.utils.*
+
+/**
+ * Given a [point] with known normalized coordinates, rotate it by [angleDegree] around the current
+ * centroid.
+ */
+suspend fun MapState.rotatePoint(point: Point, angleDegree: AngleDegree): Point {
+    return with(zoomPanRotateState) {
+        awaitLayout()
+
+        val xAxisScale = fullHeight / fullWidth.toDouble()
+        val scaledCenterX = centroidX / xAxisScale
+
+        val xR = rotateCenteredX(
+            point.x / xAxisScale, point.y, scaledCenterX, centroidY, angleDegree.toRad()
+        ) * xAxisScale
+        val yR = rotateCenteredY(
+            point.x / xAxisScale, point.y, scaledCenterX, centroidY, angleDegree.toRad()
+        )
+
+        Point(xR, yR)
+    }
+}
