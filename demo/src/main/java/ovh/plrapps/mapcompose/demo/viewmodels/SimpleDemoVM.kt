a6a2c5128a886e1f8be33aa40aeb575576b86724
plr <plr.devs@gmail.com>
2021-12-14 12:38:35 +0100

    Add initialValues constructor parameter

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/viewmodels/SimpleDemoVM.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/viewmodels/SimpleDemoVM.kt
@@ -5,10 +5,9 @@ import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import ovh.plrapps.mapcompose.api.*
 import ovh.plrapps.mapcompose.demo.providers.makeTileStreamProvider
+import ovh.plrapps.mapcompose.ui.state.InitialValues
 import ovh.plrapps.mapcompose.ui.state.MapState
 
 class SimpleDemoVM(application: Application) : AndroidViewModel(application) {
@@ -18,13 +17,13 @@ class SimpleDemoVM(application: Application) : AndroidViewModel(application) {
     private val tileStreamProvider = makeTileStreamProvider(appContext)
 
     val state: MapState by mutableStateOf(
-        MapState(4, 4096, 4096).apply {
+        MapState(
+            4, 4096, 4096,
+            initialValues = InitialValues().scroll(0.5, 0.5).scale(1.2f)
+        ).apply {
             addLayer(tileStreamProvider)
             shouldLoopScale = true
             enableRotation()
-            viewModelScope.launch {
-                scrollTo(0.5, 0.5, 1f)
-            }
         }
     )
 }
--- mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/state/MapState.kt
+++ mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/state/MapState.kt
@@ -3,7 +3,9 @@ package ovh.plrapps.mapcompose.ui.state
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
 import kotlinx.coroutines.*
 import ovh.plrapps.mapcompose.core.*
 import ovh.plrapps.mapcompose.core.VisibleTilesResolver
@@ -32,15 +34,15 @@ class MapState(
     fullHeight: Int,
     tileSize: Int = 256,
     workerCount: Int = Runtime.getRuntime().availableProcessors() - 1,
-    magnifyingFactor: Int = 0,
-    highFidelityColors: Boolean = true
+    highFidelityColors: Boolean = true,
+    initialValues: InitialValues? = null
 ) : ZoomPanRotateStateListener {
     internal val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
     internal val zoomPanRotateState = ZoomPanRotateState(fullWidth, fullHeight, this)
     internal val markerState = MarkerState()
     internal val pathState = PathState()
     internal val visibleTilesResolver =
-        VisibleTilesResolver(levelCount, fullWidth, fullHeight, tileSize, magnifyingFactor) {
+        VisibleTilesResolver(levelCount, fullWidth, fullHeight, tileSize) {
             zoomPanRotateState.scale
         }
     internal val tileCanvasState = TileCanvasState(
@@ -61,6 +63,11 @@ class MapState(
     internal var touchDownCb: (() -> Unit)? = null
     internal var mapBackground by mutableStateOf(Color.White)
     internal var isFilteringBitmap: () -> Boolean by mutableStateOf({ true })
+    private var initialValues: InitialValues? = null
+
+    init {
+        this.initialValues = initialValues
+    }
 
     /**
      * Cancels all internal tasks.
@@ -72,6 +79,14 @@ class MapState(
     }
 
     override fun onStateChanged() {
+        /* Apply initial values once */
+        initialValues?.also {
+            if (zoomPanRotateState.layoutSize != IntSize.Zero) {
+                initialValues = null
+                applyInitialValues(it)
+            }
+        }
+
         renderVisibleTilesThrottled()
         stateChangeListener?.invoke(this)
     }
@@ -102,5 +117,74 @@ class MapState(
             bottom = top + zoomPanRotateState.layoutSize.height + padding * 2
             angleRad = zoomPanRotateState.rotation.toRad()
         }
+    }
+
+    private fun applyInitialValues(initialValues: InitialValues) = with(initialValues) {
+        /* Apply scale */
+        zoomPanRotateState.setScale(scale)
+
+        /* Apply scroll */
+        with(zoomPanRotateState) {
+            val offsetX = screenOffset.x * layoutSize.width
+            val offsetY = screenOffset.y * layoutSize.height
+
+            val destScrollX = (x * fullWidth * scale + offsetX).toFloat()
+            val destScrollY = (y * fullHeight * scale + offsetY).toFloat()
+
+            setScroll(destScrollX, destScrollY)
+        }
+
+        /* Apply magnifying factor */
+        visibleTilesResolver.magnifyingFactor = magnifyingFactor
+    }
+}
+
+/**
+ * Builder for initial values.
+ * Note that initial values are constrained. When constraints apis such as minimum scale or max scale
+ * are applied right after [MapState] creation, these constraints are taken into account.
+ * In the following example, the init scale will be 4f since the max scale is later set to 4f.
+ *
+ * ```
+ * MapState(4, 4096, 4096,
+ *   initialValues = InitialValues().scale(8f)
+ * ).apply {
+ *   addLayer(tileStreamProvider)
+ *   maxScale = 4f
+ * }
+ * ```
+ */
+class InitialValues {
+    internal var x = 0.0
+    internal var y = 0.0
+    internal var screenOffset: Offset = Offset(-0.5f, -0.5f)
+    internal var scale = 1f
+    internal var magnifyingFactor = 0
+
+    fun scroll(x: Double, y: Double): InitialValues {
+        this.x = x
+        this.y = y
+        return this
+    }
+
+    fun scroll(x: Double, y: Double, screenOffset: Offset): InitialValues {
+        this.screenOffset = screenOffset
+        return scroll(x, y)
+    }
+
+    fun scale(scale: Float): InitialValues {
+        this.scale = scale
+        return this
+    }
+
+    /**
+     * Alters the level at which tiles are picked for a given scale. By default, the level
+     * immediately higher (in index) is picked, to avoid sub-sampling. This corresponds to a
+     * [magnifyingFactor] of 0. The value 1 will result in picking the current level at a given scale,
+     * which will be at a relative scale between 1.0 and 2.0
+     */
+    fun magnifyingFactor(magnifyingFactor: Int): InitialValues {
+        this.magnifyingFactor = magnifyingFactor.coerceAtLeast(0)
+        return this
     }
 }
