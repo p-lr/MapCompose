cb315dc10584933a01160e67b6d5bb8f98fd3c23
plr <plr.devs@gmail.com>
2024-11-02 08:34:30 +0100

    Update Compose BOM 2024.10.01, and enable edge-to-edge on api < 35

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/MainActivity.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/MainActivity.kt
@@ -3,6 +3,7 @@ package ovh.plrapps.mapcompose.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ovh.plrapps.mapcompose.demo.ui.MapComposeDemoApp
@@ -10,6 +11,7 @@ import ovh.plrapps.mapcompose.demo.ui.theme.MapComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MapComposeTheme {
