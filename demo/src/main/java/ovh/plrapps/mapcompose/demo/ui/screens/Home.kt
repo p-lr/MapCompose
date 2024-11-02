58aff197613120a4d554a93cb66d2ba370ef8dcc
plr <plr.devs@gmail.com>
2024-11-02 09:00:42 +0100

    More compact demo home

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/Home.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/Home.kt
@@ -2,21 +2,19 @@
 
 package ovh.plrapps.mapcompose.demo.ui.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import ovh.plrapps.mapcompose.demo.R
 import ovh.plrapps.mapcompose.demo.ui.MainDestinations
 
@@ -30,20 +28,17 @@ fun Home(demoListState: LazyListState, onDemoSelected: (dest: MainDestinations) 
         }
     ) { padding ->
         LazyColumn(
-            Modifier.padding(padding),
-            state = demoListState
+            Modifier.padding(padding).fillMaxWidth(),
+            state = demoListState,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            MainDestinations.values().map { dest ->
+            MainDestinations.entries.map { dest ->
                 item {
-                    Text(
-                        text = dest.title,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { onDemoSelected.invoke(dest) }
-                            .padding(16.dp),
-                        textAlign = TextAlign.Center
-                    )
-                    Divider(thickness = 1.dp)
+                    Button(
+                        onClick = { onDemoSelected.invoke(dest) }
+                    ) {
+                        Text(text = dest.title)
+                    }
                 }
             }
         }
