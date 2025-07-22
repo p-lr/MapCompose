76fcfe3ade99b2b513d45c1119ed045a135559e4
plr <plr.devs@gmail.com>
2025-07-22 19:16:26 +0200

    The `enableMarkerDrag` api now provides the initial pointer position in `DragStartListener`

--- mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/markers/MarkerComposer.kt
+++ mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/markers/MarkerComposer.kt
@@ -40,7 +40,10 @@ internal fun MarkerComposer(
                                 Modifier.pointerInput(Unit) {
                                     detectDragGestures(
                                         onDragStart = {
-                                            data.dragStartListener?.onDragStart(data.id, data.x, data.y)
+                                            val listener = data.dragStartListener
+                                            if (listener != null) {
+                                                invokeDragStartListener(data, zoomPRState, it)
+                                            }
                                         },
                                         onDragEnd = {
                                             data.dragEndListener?.onDragEnd(data.id, data.x, data.y)
@@ -97,6 +100,32 @@ internal fun MarkerComposer(
     }
 }
 
+private fun invokeDragStartListener(
+    data: MarkerData,
+    zoomPRState: ZoomPanRotateState,
+    position: Offset
+) {
+    /* Compute the pointer offset */
+    val origin = Offset(- data.measuredWidth * data.relativeOffset.x, - data.measuredHeight * data.relativeOffset.y)
+    val pointerOffset = position - origin
+    val angle = -zoomPRState.rotation.toRad()
+    val pointerOffsetRotated = Offset(
+        rotateX(pointerOffset.x.toDouble(), pointerOffset.y.toDouble(), angle).toFloat(),
+        rotateY(pointerOffset.x.toDouble(), pointerOffset.y.toDouble(), angle).toFloat()
+    )
+
+    val px = data.x + pointerOffsetRotated.x.toDouble() / (zoomPRState.fullWidth * zoomPRState.scale)
+    val py = data.y + pointerOffsetRotated.y.toDouble() / (zoomPRState.fullHeight * zoomPRState.scale)
+
+    data.dragStartListener?.onDragStart(
+        id = data.id,
+        x = data.x,
+        y = data.y,
+        px = if (data.isConstrainedInBounds) px.coerceIn(0.0, 1.0) else px,
+        py = if (data.isConstrainedInBounds) py.coerceIn(0.0, 1.0) else py
+    )
+}
+
 private fun invokeDragInterceptor(
     data: MarkerData,
     zoomPRState: ZoomPanRotateState,
@@ -108,10 +137,8 @@ private fun invokeDragInterceptor(
     val dx = rotateX(deltaPx.x.toDouble(), deltaPx.y.toDouble(), angle)
     val dy = rotateY(deltaPx.x.toDouble(), deltaPx.y.toDouble(), angle)
 
-    val deltaX =
-        (data.x + dx / (zoomPRState.fullWidth * zoomPRState.scale)).coerceIn(0.0, 1.0) - data.x
-    val deltaY =
-        (data.y + dy / (zoomPRState.fullHeight * zoomPRState.scale)).coerceIn(0.0, 1.0) - data.y
+    val deltaX = dx / (zoomPRState.fullWidth * zoomPRState.scale)
+    val deltaY = dy / (zoomPRState.fullHeight * zoomPRState.scale)
 
     /* Compute the pointer offset */
     val origin = Offset(- data.measuredWidth * data.relativeOffset.x, - data.measuredHeight * data.relativeOffset.y)
@@ -124,13 +151,13 @@ private fun invokeDragInterceptor(
     val px = data.x + pointerOffsetRotated.x.toDouble() / (zoomPRState.fullWidth * zoomPRState.scale)
     val py = data.y + pointerOffsetRotated.y.toDouble() / (zoomPRState.fullHeight * zoomPRState.scale)
 
-    with(data) {
-        dragInterceptor?.onMove(
-            id, x, y,
-            deltaX,
-            deltaY,
-            if (data.isConstrainedInBounds) px.coerceIn(0.0, 1.0) else px,
-            if (data.isConstrainedInBounds) py.coerceIn(0.0, 1.0) else py
-        )
-    }
+    data.dragInterceptor?.onMove(
+        id = data.id,
+        x = data.x,
+        y = data.y,
+        dx = deltaX,
+        dy = deltaY,
+        px = if (data.isConstrainedInBounds) px.coerceIn(0.0, 1.0) else px,
+        py = if (data.isConstrainedInBounds) py.coerceIn(0.0, 1.0) else py
+    )
 }
--- mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/state/markers/MarkerRenderState.kt
+++ mapcompose/src/main/java/ovh/plrapps/mapcompose/ui/state/markers/MarkerRenderState.kt
@@ -185,10 +185,10 @@ fun interface DragStartListener {
     /**
      * @param id: The id of the marker
      * @param x, y: The normalized coordinates of the marker, before the drag starts.
-     *
-     * This api may evolve to also include the pointer position when the drag starts.
+     * @param px, py: The current normalized coordinates of the pointer. If the marker's
+     * "isConstrainedInBounds" property is set to true, these coordinates are coerced in 0.0..1.0
      */
-    fun onDragStart(id: String, x: Double, y: Double)
+    fun onDragStart(id: String, x: Double, y: Double, px: Double, py: Double)
 }
 
 fun interface DragEndListener {
